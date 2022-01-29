5f4f4dd23587cb6b27b8b3eecc390b44d7ab1bd6
Burmuruk <santoyo.herrera.david@gmail.com>
2022-01-29 08:53:33 -0600

    Propuesta de movimientos básicos
    Movimiento lateral, salto, detección de muros, suelo y plataformas y dash (sin terminar).

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,232 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BoxDimentions
+{
+    public Vector2 centre;
+    public Vector2 extends;
+
+    public BoxDimentions(Vector2 centre, Vector2 extends)
+    {
+        this.centre = centre;
+        this.extends = extends;
+    }
+}
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    PlayerManager1 manager1 = null;
+    Rigidbody2D rgb2d;
+    BoxCollider2D boxCollider;
+    Animator animator;
+    Vector2 dashVelocity = default;
+    
+    Vector2 currentDirection = default;
+    bool inDash = false;
+    public bool isGrounded = false;
+    BoxCollider2D colliderToAvoid = default;
+    (bool isTouchingWall, bool isAtRight) wallDetection;
+
+    float Speed { get => manager1.Speed; }
+    float JumpForce { get => manager1.JumpForce; }
+    float DashDistance { get => manager1.DashDistance; }
+    float DashSmooth { get => manager1.DashSmooth; }
+    public List<string> TagsToAvoid { get => manager1.TagsToAvoid; }
+
+    private void Awake()
+    {
+        manager1 = GameObject.FindObjectOfType<PlayerManager1>();
+        rgb2d = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //if (!Check_Platform())
+        //    Check_Ground();
+        Check_Ground();
+
+        if (!isGrounded) Check_Platform();
+
+        Check_Wall();
+    }
+
+    private bool Check_Platform()
+    {
+        if(!isGrounded && colliderToAvoid)
+        {
+            if (boxCollider.bounds.min.y >= colliderToAvoid.bounds.max.y)
+            {
+                Ignore_Collition(false);
+                colliderToAvoid = default;
+            }
+
+            //Check_WayToMove(Vector2.down);
+            //isGrounded = true;
+        }
+
+        return false;
+    }
+
+    public bool InteractWith_Movement(float direction)
+    {
+        if (!Move(direction))
+            return false;
+
+        currentDirection.x = direction;
+        return true;
+    }
+
+    public void InteractWith_Dash()
+    {
+        if (inDash)
+            return;
+
+        float? distance = Check_WayToMove(currentDirection, DashDistance);
+
+        if (distance.HasValue)
+            Dash(distance.Value);
+    }
+
+    public void InteractWith_Jump()
+    {
+        if (!isGrounded || inDash) return;
+
+        float? distance = Check_WayToMove(Vector2.up, 1f, TagsToAvoid);
+
+        if (distance.HasValue && colliderToAvoid)
+        {
+            Jump();
+            Ignore_Collition();
+        }
+        else
+            Jump();
+    }
+
+    private void Ignore_Collition(bool shouldIgnore = true) => Physics2D.IgnoreCollision(boxCollider, colliderToAvoid, shouldIgnore);
+
+    void Check_Ground()
+    {
+        float? distance = Check_WayToMove(Vector2.down);
+
+        if (distance.HasValue)
+        {
+            if (distance.Value <= .5f)
+                isGrounded = true;
+        }
+        else
+            isGrounded = false;
+    }
+
+    void Check_Wall()
+    {
+        float? distance = Check_WayToMove(currentDirection, .1f, TagsToAvoid);
+
+        if (distance.HasValue && distance <= 0)
+            wallDetection.isTouchingWall = true;
+        else
+            wallDetection.isTouchingWall = false;
+
+        wallDetection.isAtRight = (currentDirection.x >= 0);
+    }
+
+    bool Move(float direction)
+    {
+        if (inDash) return false;
+
+        if (!wallDetection.isTouchingWall || (wallDetection.isTouchingWall && ((direction >= 0 && wallDetection.isAtRight) || !(direction >= 0 && wallDetection.isAtRight))))
+        {
+            transform.Translate(direction * Speed * Time.deltaTime, 0, 0);
+        }
+
+        return true;
+    }
+
+    void Jump()
+    {
+        rgb2d.velocity = new Vector2(rgb2d.velocity.x, JumpForce);
+    }
+
+    float? Check_WayToMove(Vector2 direction, float offset = .1f, List<string> tagsToAvoid = null)
+    {
+        RaycastHit2D[] hit2D;
+
+        Vector2 offset1 = direction * offset;
+        Vector3 startPosition = new Vector3()
+        {
+            x = transform.position.x + offset1.x,
+            y = transform.position.y + offset1.y,
+            z = 0
+        };
+
+        hit2D = Physics2D.BoxCastAll(startPosition, boxCollider.size, 0, currentDirection);
+
+        if (hit2D.Length != 0)
+        {
+            foreach (RaycastHit2D hit in hit2D)
+            {
+                if (tagsToAvoid != null && tagsToAvoid.Count != 0)
+                {
+                    bool founded = false;
+                    foreach (string tag in tagsToAvoid)
+                    {
+                        if (hit.transform.tag == tag)
+                        {
+                            founded = true;
+                            break;
+                        }
+                    }
+
+                    if (founded && !colliderToAvoid)
+                        colliderToAvoid = hit.transform.GetComponent<BoxCollider2D>();
+                    else if (founded && colliderToAvoid == hit.transform.GetComponent<BoxCollider2D>())
+                        colliderToAvoid = hit.transform.GetComponent<BoxCollider2D>();
+                    else if (!founded && colliderToAvoid)
+                        colliderToAvoid = default;
+                }
+
+                if (hit.transform.gameObject == gameObject) continue;
+
+                if (Vector2.Angle(direction *-1, hit.normal) < 30)
+                    return hit.distance;
+            }
+        }
+
+        return null;
+    }
+
+    private void Dash(float finalDistance)
+    {
+        Vector2 finalTarget = default;
+
+        if (finalDistance > DashDistance)
+            finalTarget = new Vector2(transform.position.x + DashDistance * currentDirection.x, transform.position.y);
+        else
+            finalTarget = new Vector2(transform.position.x + finalDistance, transform.position.y);
+
+        StartCoroutine(DashTo(finalTarget));
+    }
+
+    IEnumerator DashTo(Vector2 final)
+    {
+        Vector2 target;
+        transform.position = Vector2.SmoothDamp(transform.position, final, ref dashVelocity, DashSmooth, Speed);
+        inDash = true;
+        float curentTime = 0;
+
+        while (curentTime < DashSmooth)
+        {
+            transform.position = Vector2.SmoothDamp(transform.position, final, ref dashVelocity, DashSmooth, Speed);
+            yield return new WaitForEndOfFrame();
+            curentTime += Time.deltaTime;
+        }
+
+        dashVelocity = default;
+        inDash = false;
+        yield break;
+    }
+}
---
+++ Assets/Scripts/PlayerManager1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager1 : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] PlayerController[] players = default;
+    [SerializeField] List<string> tagsToAvoid = default;
+
+    [Header("Configuration")]
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] float dashDistance = 5;
+    [SerializeField] float dashTime = 3;
+    float xDirection = 0;
+
+    public float Speed { get => speed; }
+    public float JumpForce { get => jumpForce; }
+    public float DashDistance { get => dashDistance; }
+    public float DashSmooth { get => dashTime; }
+    public List<string> TagsToAvoid { get => tagsToAvoid; }
+
+    void Update()
+    {
+        xDirection = Input.GetAxisRaw("Horizontal");
+
+        if (xDirection != 0){
+            foreach (PlayerController player in players)
+            {
+                player.InteractWith_Movement(xDirection);
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            foreach (PlayerController player in players)
+            {
+                player.InteractWith_Jump();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            foreach (PlayerController player in players)
+            {
+                player.InteractWith_Dash();
+            }
+        }
+    }
+}
